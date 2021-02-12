5bc1d871275c78f86ac923a39c0908e747074152
SCBee <77908243+SCBee@users.noreply.github.com>
2021-02-12 13:40:52 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "define/stdafx.h"
+#include "api/xor.h"
+#include "api/api.h"
+#include "driver/driver.h"
+#include "inject/injector.h"
+#include "api/drvutils.h"
+
+int main()
+{
+	start_driver();
+	cout << endl;
+
+	inject_the_honey(xor_a("The Hive (Window Name)"), xor_w(L"The Honey (dll)"));
+
+	cout << endl;
+	system("pause");
+}
